f41328c2b89ea6472cc79d190165f9a36cac7e5f
Alexei Afanasyeu <behemoth-13@yandex.ru>
2020-07-08 10:49:36 +0300

    setup main activity ui

--- app/src/main/java/by/afanasyeu/catapp/activity/MainActivity.kt
+++ app/src/main/java/by/afanasyeu/catapp/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package by.afanasyeu.catapp
+package by.afanasyeu.catapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.afanasyeu.catapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
